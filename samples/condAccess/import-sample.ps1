2ef6860fb5a16acac7b799828f04ba41010a118e
Hans-Carl Oberdalhoff <78223857+hcoberdalhoff@users.noreply.github.com>
2021-04-11 22:04:30 +0200

    formatting

--- samples/condAccess/import-sample.ps1
+++ samples/condAccess/import-sample.ps1
@@ -1,1 +1,3 @@
-Get-ChildItem -Path "*.json" | ForEach-Object { $object = Import-PolicyObject -filename $_.Name; Add-ConditionalAccessPolicy -policy $object }
+Get-ChildItem -Path "*.json" | ForEach-Object { 
+  $object = Import-PolicyObject -filename $_.Name; Add-ConditionalAccessPolicy -policy $object 
+}
