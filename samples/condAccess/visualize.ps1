742f933321dc54a5b7fc852054dc9344f6daa9ff
Hans-Carl Oberdalhoff <hans-carl.oberdalhoff@primepulse.de>
2022-02-17 16:06:57 +0100

    better explain permissions for visualizing

--- samples/condAccess/visualize.ps1
+++ samples/condAccess/visualize.ps1
@@ -1,4 +1,11 @@
 # This will create a "Mermaid in HTML" visualization for a given Cond. Access Policy
+# You will need the following permission at least:
+# - Policy.Read.All
+# To also resolve object name, also you will need:
+# - Application.Read.All [for apps to be queried]# 
+# - GroupMember.Read.All [for group names to be resolved]
+# - User.Read [default/delegated permission]
+# - User.Read.All [for user names to be resolved]
 
 # Read the policy
 $pol = Import-PolicyObject -filename .\Demo_CAP.json
